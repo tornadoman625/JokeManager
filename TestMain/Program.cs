fe67511c24e8b1b7da04602c4d9078c2267b1250
nicomp42 <nicholdw@ucmail.uc.edu>
2021-11-22 12:31:31 -0500

    It's the final project!

--- TestMain/Program.cs
+++ TestMain/Program.cs
@@ -11,6 +11,7 @@ namespace TestMain
     {
         static void Main(string[] args)
         {
+            // This runs...
             int status = JokeManager.SaveJoke("test", "test1");
         }
     }
